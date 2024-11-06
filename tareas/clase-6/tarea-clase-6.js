d1fefab6965e555ee1527e84304a31a91a3d035d
MeatSpin360 <emiliobrescasin@gmail.com>
2024-11-06 16:39:26 -0300

    intento de form para carga manual de paciente

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -7,6 +7,32 @@ Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuev
 */
 
 
+
+document.querySelector('#paciente-nuevo').onclick = function(event) {
+    document.querySelector('#NombreDelPaciente').className = "";
+   
+    
+    event.preventDefault();
+};
+
+document.querySelector('#nombreOK').onclick = function(event) {
+    document.querySelector('#DatosPersonales').className = "";
+
+    event.preventDefault();
+};
+
+const info = document.querySelector(".alert-info");
+
+function process(event) {
+    event.preventDefault();
+    const phoneNumber = phoneInput.getNumber();
+    info.style.display = "";
+    info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
+
+    
+};
+
+
 /*
 TAREA:
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
